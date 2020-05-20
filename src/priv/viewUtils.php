b48b1ba2df4528a7fa23795a07c04e2271770a50
Daryl Anthony Chouinard <daryl@darlo.me>
2020-05-20 03:43:18 -0400

    fix toLink so it doesn't add random /

--- src/priv/viewUtils.php
+++ src/priv/viewUtils.php
@@ -2,7 +2,7 @@
 function toLink(string $link, string $currentPage) {
     if(substr($link, 0, 1) === '/') {
         $count = substr_count($currentPage, '/', 1);
-        return './' . join('/', array_fill(0, $count, '..')) . $link;
+        return '.' . str_repeat('/..', $count) . $link;
     }
     return $link;
 }
